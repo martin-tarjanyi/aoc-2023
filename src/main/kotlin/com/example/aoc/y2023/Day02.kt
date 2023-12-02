3b4ddef9936616623739cd8930d530497e666fe0
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2023-12-02 15:25:14 +0100

    Day 2 part 2

--- src/main/kotlin/com/example/aoc/y2023/Day02.kt
+++ src/main/kotlin/com/example/aoc/y2023/Day02.kt
@@ -1,16 +1,31 @@
 package com.example.aoc.y2023
 
 fun main() {
-    val bag = Bag(mapOf(
-        Color.RED to 12,
-        Color.GREEN to 13,
-        Color.BLUE to 14,
-    ))
+    part1()
+    part2()
+}
+
+private fun part1() {
+    val bag = Bag(
+        mapOf(
+            Color.RED to 12,
+            Color.GREEN to 13,
+            Color.BLUE to 14,
+        )
+    )
 
     inputLineSequence("day02.txt")
         .map { line -> parseGame(line) }
         .filter { game -> bag.isPossible(game) }
         .sumOf { game -> game.id }
+        .let { println(it) }
+}
+
+private fun part2() {
+    inputLineSequence("day02.txt")
+        .map { line -> parseGame(line) }
+        .map { game -> game.smallestPossibleBag() }
+        .sumOf { bag -> bag.power() }
         .let { println(it) }
 }
 
@@ -33,9 +48,16 @@ private data class Bag(val cubes: Map<Color, Long>) {
     fun isPossible(game: Game): Boolean = game.reveals.all { reveal ->
         reveal.cubes.all { (color, num) -> num <= (this.cubes[color] ?: 0) }
     }
+
+    fun power(): Long = cubes.values.reduce { a, b -> a * b }
 }
 
-private data class Game(val id: Long, val reveals: List<RevealedSubsets>)
+private data class Game(val id: Long, val reveals: List<RevealedSubsets>) {
+    fun smallestPossibleBag(): Bag = Color.entries
+        .associateWith { color -> reveals.maxOf { reveal -> reveal.cubes[color] ?: 0 } }
+        .let { Bag(it) }
+}
+
 private data class RevealedSubsets(val cubes: Map<Color, Long>)
 
 private enum class Color {
