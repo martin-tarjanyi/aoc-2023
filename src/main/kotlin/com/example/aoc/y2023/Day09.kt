32be6f24e582c23275c97d7279dd32059e54d9d8
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2023-12-18 14:55:24 +0100

    Day 9 - part 2

--- src/main/kotlin/com/example/aoc/y2023/Day09.kt
+++ src/main/kotlin/com/example/aoc/y2023/Day09.kt
@@ -2,29 +2,57 @@ package com.example.aoc.y2023
 
 fun main() {
     part1()
+    part2()
 }
 
 private fun part1() {
     inputLineSequence("day09.txt")
         .sumOf { line ->
             val nums = line.split(" ").map { it.toLong() }
-            val sequenceHistory = mutableListOf(nums)
-            var currentSequence = nums
-            while (currentSequence.any { it != 0L }) {
-                currentSequence = currentSequence.windowed(2)
-                    .map { (a, b) -> b - a }
-                sequenceHistory.add(currentSequence)
-            }
+            val sequenceHistory = sequenceHistory(nums)
 
             sequenceHistory
                 .reversed()
                 .drop(1)
-                .reduce { acc, longs ->
-                    val increment = acc.last()
-                    longs + (longs.last() + increment)
+                .reduce { previousSequence, nextSequence ->
+                    val increment = previousSequence.last()
+                    nextSequence + (nextSequence.last() + increment)
                 }
                 .last()
         }
         .let { println(it) }
+}
+
+private fun part2() {
+    inputLineSequence("day09.txt")
+        .sumOf { line ->
+            val nums = line.split(" ").map { it.toLong() }
+            val sequenceHistory = sequenceHistory(nums)
+
+            sequenceHistory
+                .reversed()
+                .drop(1)
+                .reduce { previousSequence, nextSequence ->
+                    val increment = previousSequence.first()
+                    buildList {
+                        add(nextSequence.first() - increment)
+                        addAll(nextSequence)
+                    }
+                }
+                .first()
+        }
+        .let { println(it) }
+}
+
+private fun sequenceHistory(originalSequence: List<Long>): MutableList<List<Long>> {
+    val nums = originalSequence
+    val sequenceHistory = mutableListOf(nums)
+    var currentSequence = nums
+    while (currentSequence.any { it != 0L }) {
+        currentSequence = currentSequence.windowed(2)
+            .map { (a, b) -> b - a }
+        sequenceHistory.add(currentSequence)
+    }
+    return sequenceHistory
 }
 
